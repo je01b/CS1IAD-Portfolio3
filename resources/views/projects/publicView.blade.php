4a824882d5a7589703d8e2807e58948bd5a99916
Jack B <jack@jack-b.me>
2023-04-21 21:52:36 +0100

    Update publicView.blade.php

--- resources/views/projects/publicView.blade.php
+++ resources/views/projects/publicView.blade.php
@@ -51,9 +51,7 @@
            <strong>Description: </strong> {{ $project->description }}
            <br>
            <br>
-           <h3 class="text-2xl font-semibold dark:text-white">Assigned User Details:</h3>
-           <strong>User ID: </strong> {{ $project->user_id }}
-           <br>
+           <h3 class="text-2xl font-semibold dark:text-white">User Details:</h3>
            <strong>Name: </strong> {{ $user->name }}
            <br>
            <strong>Email: </strong> <a href="mailto:{{ $user->email}}" class="text-blue-600 visited:text-purple-600 underline" > {{ $user->email }}</a>
